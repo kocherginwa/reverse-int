12f3860249b05b1533ca8bcaef7228b727e157bd
kocherginwa <kocherginwa@gmail.com>
2022-01-07 03:03:05 +0100

    feat: implemented functionality of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    return parseInt(n.toString().split('').reverse().join(''));
 }
+
+/* function reverse (n) {
+   return parseInt(n.toString().split('').reverse().join(''));
+}
+
+console.log('reverse (12) equals: ' + reverse(12)); */
+
